235e35777354905ecf052d59462e0498265300d4
AdamUrbaniak <adamurbaniak94@gmail.com>
2023-10-29 17:49:46 +0100

    zad 0 oraz zad 1

--- nowe zadanko.py
+++ nowe zadanko.py
@@ -1,9 +1,16 @@
-def suma_od_1_do_n(n):
+import time
+
+def suma_od_1_do_n():
     n = int(input("Podaj liczbę n: "))
     suma = 0
     for i in range(1, n + 1):
         suma += i
     return suma
 
-wynik = suma_od_1_do_n(0)
+start_time = time.time()
+wynik = suma_od_1_do_n()
+end_time = time.time()
+
+execution_time = end_time - start_time
 print("Suma od 1 do n wynosi:", wynik)
+print("Czas wykonania funkcji:", execution_time, "sekundy")
